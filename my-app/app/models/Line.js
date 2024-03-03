ce6c39dd8582c3310fd444a02cb10256d054c360
IamDarian <silisteanudarian@gmail.com>
2024-03-03 14:48:58 +0200

    Finalized mongoose schema

--- my-app/app/models/Line.js
+++ my-app/app/models/Line.js
@@ -1,1 +1,50 @@
 import mongoose from "mongoose";
+
+const departureSchema = new mongoose.Schema({
+  stop: {
+    type: String,
+    required: true,
+  },
+  time: [
+    {
+      hour: {
+        type: String,
+        required: true,
+      },
+      minute: [
+        {
+          type: String,
+          required: true,
+        },
+      ],
+    },
+  ],
+});
+
+const scheduleSchema = new mongoose.Schema({
+  dayOfWeek: {
+    type: String,
+    required: true,
+  },
+  departures: [departureSchema],
+});
+
+const mainSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  validFrom: {
+    type: String,
+    required: true,
+  },
+  direction: {
+    type: String,
+    required: true,
+  },
+  schedule: [scheduleSchema],
+});
+
+const Line = mongoose.model("Line", mainSchema);
+
+module.exports = Line;
