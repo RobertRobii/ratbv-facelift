ef1719da1b38caed94d036ba30b174efeb28f4f7
RobertRobii <robertraiovici01@gmail.com>
2024-03-17 16:58:17 +0200

    conatct page partialy done

--- my-app/app/contact/page.jsx
+++ my-app/app/contact/page.jsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import Loading from "@/components/Loading";
+import ContactForm from "@/components/Contact/ContactForm";
+import ContactInformation from "@/components/Contact/ContactInformation";
 
 const Contact = () => {
   const [loading, setLoading] = useState(true);
@@ -12,43 +14,6 @@ const Contact = () => {
     }, 1000);
   }, []);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch("/api/sendEmail", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          message: message,
-        }),
-      });
-
-      if (!response.ok) {
-        console.error("Error while sending data!");
-      }
-
-      const data = await response.json();
-
-      if (data.success) {
-        alert("Email sent successfully!");
-        setName("");
-        setEmail("");
-        setMessage("");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
   return (
     <div>
       {loading ? (
@@ -56,71 +21,11 @@ const Contact = () => {
       ) : (
         <div>
           <section className="container mx-auto">
-            <div className="pt-[100px] pb-[100px]">
-              <form
-                onSubmit={handleSubmit}
-                className="max-w-sm mx-auto flex flex-col justify-center items-center md:justify-start md:items-start"
-              >
-                <h1 className="text-xl mb-5 text-white bg-accent px-4 py-2 rounded-lg">
-                  Contact Us
-                </h1>
-                <div className="mb-5">
-                  <label
-                    htmlFor="name"
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                  >
-                    Your name
-                  </label>
-                  <input
-                    onChange={(e) => setName(e.target.value)}
-                    type="text"
-                    id="name"
-                    value={name}
-                    className="bg-white text-[16px] border border-slate-300 rounded focus:drop-shadow-md focus:outline-none focus:ring
-                focus:ring-[#00B906]/50 rounded-lg pl-2 pr-3 py-2 w-[250px] md:w-[350px]"
-                    required
-                  />
-                </div>
-                <div className="mb-5">
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    id="email"
-                    value={email}
-                    className="bg-white text-[16px] border border-slate-300 rounded focus:drop-shadow-md focus:outline-none focus:ring
-                focus:ring-[#00B906]/50 rounded-lg pl-2 pr-3 py-2 w-[250px] md:w-[350px]"
-                    required
-                  />
-                </div>
-                <div className="mb-5">
-                  <label
-                    htmlFor="message"
-                    className="block mb-2 text-sm font-medium text-gray-700"
-                  >
-                    Your message
-                  </label>
-                  <textarea
-                    onChange={(e) => setMessage(e.target.value)}
-                    id="message"
-                    value={message}
-                    rows="6"
-                    className="bg-white text-[16px] border border-slate-300 rounded focus:drop-shadow-md focus:outline-none focus:ring
-                focus:ring-[#00B906]/50 rounded-lg pl-2 pr-3 py-2 w-[250px] md:w-[350px]"
-                  ></textarea>
-                </div>
-                <button
-                  type="submit"
-                  className="text-accent bg-transparent border border-accent w-[220px] px-4 py-2 rounded-lg hover:bg-accent hover:text-white transition-all duration-200"
-                >
-                  Send message
-                </button>
-              </form>
+            <div className="pt-[60px] md:pt-[120px] pb-[100px]">
+              <div className="flex flex-col-reverse xl:flex-row justify-between">
+                <ContactForm />
+                <ContactInformation />
+              </div>
             </div>
           </section>
         </div>
---
+++ my-app/components/Contact/ContactInformation.jsx
@@ -0,0 +1,100 @@
+import { PiGarageFill } from "react-icons/pi";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaCalendarAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+
+const ContactInformation = () => {
+  return (
+    <div className="text-sm md:text-lg px-[20px] xl:pl-[100px] mt-10 xl:mt-0">
+      <h1 className="text-xl mb-5 text-white text-center bg-accent px-4 py-2 rounded-lg w-[200px] mx-auto xl:ms-0">
+        Informatii contact
+      </h1>
+      <div className="flex flex-col xl:flex-row justify-center xl:justify-start items-center gap-x-2 mb-5">
+        <div className="flex items-center gap-x-2 mb-2 md:mb-0">
+          <PiGarageFill size={20} color="#00B906" />
+          <p>Nume: </p>
+        </div>
+        <p>RATBV S.A.</p>
+      </div>
+      <div className="flex flex-col xl:flex-row justify-center xl:justify-start items-center gap-x-2 mb-5">
+        <div className="flex items-center gap-x-2 mb-2 md:mb-0">
+          <FaLocationDot size={20} color="#00B906" />
+          <p>Adresa:</p>
+        </div>
+        <p>Str. Harmanului nr. 49, Brasov, Romania</p>
+      </div>
+      <div className="flex flex-col xl:flex-row justify-center xl:justify-start items-center gap-x-2 mb-5">
+        <div className="flex items-center gap-x-2 mb-2 md:mb-0">
+          <FaCalendarAlt size={18} color="#00B906" />
+          <p>Program Registratura: </p>
+        </div>
+        <p> (luni - vineri) 8:00 - 13:00</p>
+      </div>
+      <div className="flex flex-col xl:flex-row justify-center xl:justify-start items-center gap-x-2 mb-5">
+        <div className="flex items-center gap-x-2 mb-2 md:mb-0">
+          <FaCalendarAlt size={18} color="#00B906" />
+          <p>Program Administratie:</p>
+        </div>
+        <p> (luni - vineri) 8:00 - 16:00</p>
+      </div>
+      <div className="flex flex-col xl:flex-row justify-center xl:justify-start items-center gap-x-2 mb-5">
+        <div className="flex items-center gap-x-2 mb-2 md:mb-0">
+          <MdEmail size={20} color="#00B906" />
+          <p>Email: </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center">
+          <div className="flex justify-center gap-x-2">
+            <p>ratbv@ratbv.ro,</p>
+            <p>comunicare@ratbv.ro</p>
+          </div>
+          <p className="ms-0 md:ms-2">
+            (pentru sesizări și publicitate marketing)
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col xl:flex-row justify-center xl:justify-start items-center gap-x-2 mb-5">
+        <div className="flex items-center gap-x-2 mb-2 md:mb-0">
+          <FaPhoneAlt size={20} color="#00B906" />
+          <p>Telefon: </p>
+        </div>
+        <p>
+          Call center: <span>0368 800 600; </span>
+        </p>
+        <p>
+          Centrala: <span>0268 334 678, 0368 401 252; </span>
+        </p>
+        <p>
+          Fax: <span>0268 335 660</span>
+        </p>
+      </div>
+      <div className="flex items-stretch gap-x-2 mb-5">
+        <div className="flex flex-col gap-x-2 mb-3">
+          <div className="flex justify-center md:justify-start items-center gap-x-2 mb-3">
+            <FaCalendarAlt size={18} color="#00B906" />
+            <p>Program Audiente:</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="mb-3 text-center md:text-left">
+              • Director General sau înlocuitor - în fiecare săptămână, vineri,
+              între orele 10.00-12.00
+            </p>
+            <p className="mb-3 text-center md:text-left">
+              • Director Tehnic sau înlocuitor - săptămâna 2 si 4 a lunii, marţi
+              între orele 15.00-17.00
+            </p>
+            <p className="mb-3 text-center md:text-left">
+              • Director Exploatare sau înlocuitor - săptămâna 1 și 3 a lunii,
+              miercuri orele 13:00-15:00
+            </p>
+            <p className="mb-3 text-center md:text-left">
+              • Şef Serviciu Control sau înlocuitor, joi între orele 11.00-13.00
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInformation;
